82adbc124bf72b58028f8043d4319af02c4276f1
panksxd <panksxd@gmail.com>
2023-09-05 11:37:59 +0200

    Update AllInformationUserDTO.java

--- src/main/java/dto/AllInformationUserDTO.java
+++ src/main/java/dto/AllInformationUserDTO.java
@@ -1,5 +1,6 @@
 package dto;
 
+import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import model.entities.Hobby;
@@ -11,6 +12,7 @@ import java.util.Set;
 
 @ToString
 @Setter
+@Getter
 public class AllInformationUserDTO
 {
 
