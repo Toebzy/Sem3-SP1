cff6ebf8ce8ab9e0a20ed22660a662d17947ee2d
panksxd <panksxd@gmail.com>
2023-09-05 10:32:16 +0200

    Update BigDAO.java

--- src/main/java/dao/BigDAO.java
+++ src/main/java/dao/BigDAO.java
@@ -1,6 +1,9 @@
 package dao;
 
+import jakarta.persistence.EntityManagerFactory;
+import model.config.HibernateConfig;
+
 public class BigDAO
 {
-    
+    EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryConfig("hobby");
 }
